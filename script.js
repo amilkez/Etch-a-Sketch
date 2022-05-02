fedd4fb6f474fb0f7f7c22cb44378bb1b070a621
angel <angelhidalgomb@gmail.com>
2022-05-02 09:49:59 -0500

    Add squared elements with js

---
+++ script.js
@@ -0,0 +1,11 @@
+const container = document.querySelector(".container");
+for (let i = 0; i < 16; i++) {
+	const row = document.createElement("div");
+	row.classList.add("row");
+	container.appendChild(row);
+	for (let j = 0; j < 16; j++) {
+		const block = document.createElement("div");
+		block.classList.add("block");
+		row.appendChild(block);
+	}
+}
