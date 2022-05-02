4cf1b1ffb8e1f84d06413b10e84d2ad5f9a5efea
angel <angelhidalgomb@gmail.com>
2022-05-02 10:46:52 -0500

    Remove last grid when creating a new grid

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const container = document.querySelector(".container");
 const newGridBtn = document.querySelector(".new-grid");
 
+newGridBtn.addEventListener("click", () => {
+	const gridSize = prompt("Choose a new grid size");
+	console.log(gridSize);
+	if (gridSize > 99) {
+		alert("Choose a grid size between 1 and 99");
+	}
+	removeGrid();
+	createGrid(gridSize);
+});
+
 function createGrid(gridSize = 16) {
 	for (let i = 0; i < gridSize; i++) {
 		const row = document.createElement("div");
@@ -10,18 +20,24 @@ function createGrid(gridSize = 16) {
 			const block = document.createElement("div");
 			block.classList.add("block");
 			row.appendChild(block);
-			block.addEventListener("mouseenter", () => {
-				block.classList.add("hover");
-			});
+			block.addEventListener("mouseenter", hoverRow);
 		}
 	}
 }
 
-newGridBtn.addEventListener("click", () => {
-	const gridSize = prompt("Choose a new grid size");
-	console.log(gridSize);
-	if (gridSize > 99) {
-		alert("Choose a grid size between 1 and 99");
-	}
-	createGrid(gridSize);
-});
+function removeGrid() {
+	const rows = document.querySelectorAll(".row");
+	rows.forEach((row) => {
+		const blocks = document.querySelectorAll(".block");
+		blocks.forEach((block) => {
+			block.remove();
+		});
+		row.remove();
+	});
+}
+
+function hoverRow() {
+	this.classList.add("hover-ele");
+}
+
+createGrid();
